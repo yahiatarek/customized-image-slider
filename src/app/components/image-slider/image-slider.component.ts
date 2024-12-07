9c88c5a04250d3ccde5f2e90fad1ff9d18c81a8b
yehia <yahia.yt@gmail.com>
2024-12-07 18:56:06 +0200

    editing slider temporary

--- src/app/components/image-slider/image-slider.component.ts
+++ src/app/components/image-slider/image-slider.component.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT, NgOptimizedImage } from '@angular/common';
-import { Component, Inject, InjectionToken, input, OnInit, Signal, signal } from '@angular/core';
+import { Component, Inject, input, OnInit, signal } from '@angular/core';
 import { SliderOverlayComponent } from '../slider-overlay/slider-overlay.component';
 
 @Component({
@@ -12,35 +12,42 @@ import { SliderOverlayComponent } from '../slider-overlay/slider-overlay.compone
 export class ImageSliderComponent implements OnInit {
   constructor(@Inject(DOCUMENT) private document: Document) { }
 
-  $imageElement = signal<NodeListOf<HTMLElement> | null>(null)
+  $imageElements = signal<NodeListOf<HTMLElement> | null>(null)
+  $currentImageElement = signal<HTMLElement>((this.$imageElements() as NodeListOf<HTMLElement>)?.item(0))
+
+  private options = {
+    root: this.$currentImageElement(),
+    rootMargin: "0px",
+    threshold: 1.0,
+  };
+
+  observer = new IntersectionObserver((item) => {
+    console.log(item, 'item')
+  }, this.options);
 
   ngOnInit(): void {
-    this.$imageElement.set(this.document.querySelectorAll('.item-picture') as NodeListOf<HTMLElement>);
+    this.$imageElements.set(this.document.querySelectorAll('.item-picture') as NodeListOf<HTMLElement>);
   }
 
   $items = input.required<string[]>();
 
   protected imageTranslation = signal('0')
 
-
-
   protected getSrc(item: string, breakpoint: number): string {
     return item.replace(/_bp\d{1,4}\.webp/, `_bp${breakpoint}.webp`);
   }
 
   prevImage(e: Event) {
-    this.imageTranslation.set(`${(this.$imageElement() as NodeListOf<HTMLElement>)[0].clientWidth}px`);
+    this.imageTranslation.set(`100%`);
     this.updateImageTranslation();
   }
 
   nextImage(e: Event) {
-    this.imageTranslation.set(`-${(this.$imageElement() as NodeListOf<HTMLElement>)[0].clientWidth}px`);
+    this.imageTranslation.set(`-100%`);
     this.updateImageTranslation();
   }
 
   private updateImageTranslation() {
-    this.$imageElement()?.forEach((image) => {
-      image.style.setProperty('--imageTranslation', this.imageTranslation());
-    })
+    this.$currentImageElement()?.style.setProperty('--imageTranslation', this.imageTranslation());
   }
 }
